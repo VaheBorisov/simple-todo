efbdbf67710fe98d5c5e2007314f8ddf5fe50826
Vahe <borisov.vahe@mail.ru>
2021-04-11 13:34:02 +0400

    end

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,47 @@
-import './App.css';
+import React, { useState, useEffect, useReducer } from 'react';
+import TodoList from './TodoList';
+import { Context } from "../context";
+import reducer from '../reducer'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default function App() {
+
+    const [state, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem('todos')));
+    const [todoTitle, setTodoTitle] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(state));
+    }, [state]);
+
+    const addTodo = event => {
+        if (event.key === 'Enter') {
+            dispatch({
+                type: 'add',
+                payload: todoTitle
+            })
+            setTodoTitle('')
+        }
+    };
+
+
+    return (
+        <Context.Provider value={{
+            dispatch
+        }}>
+            <div className="container">
+                <h1>Todo app</h1>
+
+                <div className="input-field">
+                    <input
+                        type="text"
+                        value={todoTitle}
+                        onChange={event => setTodoTitle(event.target.value)}
+                        onKeyPress={addTodo}
+                    />
+                    <label>Todo name</label>
+                </div>
+
+                <TodoList todos={state} />
+            </div>
+        </Context.Provider>
+    );
 }
-
-export default App;
